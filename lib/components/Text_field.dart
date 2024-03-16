30202c1d0ee79bb7997aa1a4e409f78339865925
Bhargavaansh2211 <143218592+Bhargavaansh2211@users.noreply.github.com>
2024-03-17 03:00:30 +0530

    improved MyTextField UI

--- lib/components/Text_field.dart
+++ lib/components/Text_field.dart
@@ -1,31 +1,42 @@
 import 'package:flutter/material.dart';
 
 class MyTextField extends StatelessWidget {
-  const MyTextField({super.key, required this.hinttext, required this.unknowntext,required this.controller});
+  const MyTextField({
+    Key? key,
+    required this.controller,
+    required this.hinttext,
+    required this.unknowntext,
+  }) : super(key: key);
 
   final TextEditingController controller;
   final String hinttext;
   final bool unknowntext;
 
-
-
   @override
   Widget build(BuildContext context) {
-    return TextField(
-      controller: controller,
-      obscureText: unknowntext,
-      decoration: InputDecoration(
-        enabledBorder: OutlineInputBorder(
-          borderSide: BorderSide(color: Colors.grey)
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+      child: TextField(
+        controller: controller,
+        obscureText: unknowntext,
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.symmetric(vertical: 14.0, horizontal: 16.0),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10.0),
+            borderSide: BorderSide(color: Colors.grey),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10.0),
+            borderSide: BorderSide(color: Colors.blue),
+          ),
+          filled: true,
+          fillColor: Colors.grey[200],
+          hintText: hinttext,
+          hintStyle: TextStyle(
+            color: Colors.black54,
+            fontWeight: FontWeight.w500,
+          ),
         ),
-        focusedBorder: const OutlineInputBorder(
-            borderSide: BorderSide(color: Colors.grey)
-        ),
-        fillColor:  Colors.grey[400],
-        filled: true,
-        hintText: hinttext,
-        hintStyle: TextStyle(color: Colors.black54,
-        fontWeight: FontWeight.w500),
       ),
     );
   }
