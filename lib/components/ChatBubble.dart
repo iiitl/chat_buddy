c96b86555de51ab84be1adc3d81a77725218cb20
Bhargavaansh2211 <143218592+Bhargavaansh2211@users.noreply.github.com>
2024-03-17 03:26:40 +0530

    improved the ui of chat bubble

--- lib/components/ChatBubble.dart
+++ lib/components/ChatBubble.dart
@@ -3,17 +3,30 @@ import 'package:flutter/material.dart';
 class ChatBubble extends StatelessWidget {
   final String message;
 
-  const ChatBubble({required this.message, super.key});
+  const ChatBubble({required this.message, Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(12),
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(8), color: Colors.grey[600]),
-      child: Text(
-        message,
-        style: TextStyle(fontSize: 15,  color: Colors.white),
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        padding: EdgeInsets.all(12),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          gradient: LinearGradient(
+            colors: [const Color.fromARGB(255, 182, 36, 207), Colors.lightBlue],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Text(
+          message,
+          style: TextStyle(
+            fontSize: 16,
+            color: Colors.white,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
       ),
     );
   }
