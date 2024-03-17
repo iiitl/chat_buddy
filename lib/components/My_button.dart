d7baf8bbe350d87dd5b42468b27afbd24f386126
gauravghodinde <gauravghodinde@gmail.com>
2024-03-17 22:31:09 +0530

    added the app icon and configured the pub.yaml file

--- lib/components/My_button.dart
+++ lib/components/My_button.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
 
-class MyButton extends StatelessWidget {
+class MyButton extends StatefulWidget {
   const MyButton({super.key, required this.ontap, required this.text});
 
-  final void Function () ? ontap;
+  final void Function()? ontap;
   final String text;
 
+  @override
+  State<MyButton> createState() => _MyButtonState();
+}
 
+class _MyButtonState extends State<MyButton> {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: ontap,
+      onTap: widget.ontap,
       child: Container(
         decoration: BoxDecoration(
           color: Colors.black,
@@ -19,12 +23,13 @@ class MyButton extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Center(
-            child: Text(text,
-            style: const TextStyle(
-              fontWeight: FontWeight.w500,
-              fontSize: 20,
-              color: Colors.white
-            ),),
+            child: Text(
+              widget.text,
+              style: const TextStyle(
+                  fontWeight: FontWeight.w500,
+                  fontSize: 20,
+                  color: Colors.white),
+            ),
           ),
         ),
       ),
